f9bbb238ac630d52a2e27145245ac38fad72ef6e
Roman Ackermann <roman@waprogramms.com>
2020-05-16 16:40:07 +0200

    Added service provider

---
+++ src/MsGraphMailServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace LaravelMsGraphMail;
+
+use Illuminate\Support\ServiceProvider;
+use LaravelMsGraphMail\Exceptions\CouldNotSendMail;
+
+class MsGraphMailServiceProvider extends ServiceProvider {
+
+    /**
+     * Boot any application services.
+     * @return void
+     */
+    public function boot() {
+        $this->app->get('mail.manager')->extend('microsoft-graph', function (array $config) {
+            if (!isset($config['client']) || !isset($config['secret']) || !isset($config['transport'])) {
+                throw CouldNotSendMail::invalidConfig();
+            }
+
+            return new MsGraphMailTransport($config);
+        });
+    }
+
+}
